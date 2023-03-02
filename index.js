384ba3e741c2bbf930cd222201d8c74807c44fd8
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-03-01 18:44:48 -0700

    signature verification

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { createVerify, createHmac } = require('crypto');
+const {  createHmac } = require('crypto');
+const ed = require('@noble/ed25519');
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
@@ -6,6 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 const port = 3000;
+const msg = 'DO NOT SIGN THIS UNLESS YOU ARE ON https://holonym.id';
 
 const _prf = input => {
     const hmac = createHmac('sha256', process.env.HMAC_PRF_SECRET);
@@ -14,38 +16,46 @@ const _prf = input => {
 }
 
 // Signs the ephemeral "pubkey" given as prfSeed, and signature is used as easy-to-code proof of discrete log
-const authorizedPRF = (pubkey, sig) => {
-    if(_verify(pubkey,sig)) {
+const authorizedPRF = async (pubkey, sig) => {
+    if(await _verify(pubkey,sig)) {
         return _prf(pubkey);
     } else {
         throw 'Error verifying knowledge of discrete log (in this case, via ECDSA signature)';
     }
 }
 
-const _verify = (pubkey, sig) => {
-    const verify = createVerify('SHA256');
-    verify.update('DO NOT SIGN THIS UNLESS YOU ARE ON https://holonym.id');
-    verify.end();
-    return verify.verify(pubkey, sig);
+const _verify = async (pubkey, sig) => {
+    // const verify = createVerify(null);
+    // verify.update();
+    // verify.end();
+    // return verify.verify(pubkey, sig);
+    // console.log("verifu", await ed.verify(sig, Buffer.from(msg), pubkey));
+    
+    // For some reason, bad inputs cause the promise to never reject. Timeout instead to avoid this minor bug:
+//     console.log("this was called")
+//     const result = Promise.race([
+//         ed.verify(sig, Buffer.from(msg), pubkey),
+//         new Promise(function(resolve, reject){
+//             console.log(Object.keys(resolve))
+//         })
+//     ]);
+//     console.log("rrrr", await result);
+//    return result;
+    return await ed.verify(sig, Buffer.from(msg), pubkey)
+//    return await ed.verify(sig, Buffer.from(msg), pubkey);
 }
 
 
+/* ****************** */
 
-
-
-
-
-
-
-
-
-
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
+    res.setTimeout(1000);
     console.log("body", req.body)
-  res.send(authorizedPRF(req.body.pubkey, req.body.sig));
+  res.send(await authorizedPRF(req.body.pubkey, req.body.sig));
 })
 
 app.listen(port, () => {})
 module.exports = {
-    server: app
+    server: app,
+    msg: msg
 }
--- test.js
+++ test.js
@@ -1,22 +1,33 @@
 const { expect } = require("chai");
 const request = require("supertest");
-const { server } = require("./index");
-const bodyParser = require("body-parser");
-
+const { server, msg } = require("./index");
+const ed = require('@noble/ed25519');
 
 describe("PRF Server", function() {
     before(async function() {
     });
-    it("abcaskdfjhas", async function(){
-        const r = await request(server).post('/').send({
-            pubkey: "abc",
-            sig: "abcd"
-        });
-        // console.log(r);
-    });
     after(async function(){
-        process.exit(0);
+        // process.exit(0);
     })
+    it("abcaskdfjhas", async function(){
+        // const { privateKey, publicKey } = generateKeyPairSync('ed25519');
+        const privKey = ed.utils.randomPrivateKey();
+        const pubKey = await ed.getPublicKey(privKey);
+        
+        // console.log(Buffer.from(msg).toString('hex'));
+        // let sig = await sign(null, Buffer.from('abc'), privateKey);
+
+        const sig = await ed.sign(Buffer.from(msg), privKey);
+
+        const r = await request(server).post('/').send({
+            pubkey: Buffer.from(pubKey).toString('hex'),//.replace('5','6'),
+            sig: Buffer.from(sig).toString('hex')
+            // pubkey: publicKey.export({type: 'spki', format: 'der'}).toString('hex'),
+            // sig: sig.toString('hex')
+        });
+
+       
+    });
 
 
 });
