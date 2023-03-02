a1804abd37cb39dc7c3fd30beef984cb591f3da8
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-03-01 18:54:53 -0700

    prf test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const { expect } = require("chai");
+const { expect, should } = require("chai");
+const {  createHmac } = require('crypto');
 const request = require("supertest");
 const { server, msg } = require("./index");
 const ed = require('@noble/ed25519');
@@ -9,24 +10,21 @@ describe("PRF Server", function() {
     after(async function(){
         // process.exit(0);
     })
-    it("abcaskdfjhas", async function(){
-        // const { privateKey, publicKey } = generateKeyPairSync('ed25519');
+    it("correct signature returns prf", async function(){
         const privKey = ed.utils.randomPrivateKey();
         const pubKey = await ed.getPublicKey(privKey);
-        
-        // console.log(Buffer.from(msg).toString('hex'));
-        // let sig = await sign(null, Buffer.from('abc'), privateKey);
-
+    
         const sig = await ed.sign(Buffer.from(msg), privKey);
 
         const r = await request(server).post('/').send({
             pubkey: Buffer.from(pubKey).toString('hex'),//.replace('5','6'),
             sig: Buffer.from(sig).toString('hex')
-            // pubkey: publicKey.export({type: 'spki', format: 'der'}).toString('hex'),
-            // sig: sig.toString('hex')
         });
 
-       
+        // Simulate the PRF
+        const hmac = createHmac('sha256', process.env.HMAC_PRF_SECRET); hmac.update(Buffer.from(pubKey).toString("hex")); 
+        const shouldBe = hmac.digest('hex');
+        expect(r.text).to.eq(shouldBe)
     });
 
 
